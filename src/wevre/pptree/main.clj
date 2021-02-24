80e8cf0805e173e5d277ea292319ac2c8044cfe4
wevre <6877948+wevre@users.noreply.github.com>
2021-02-24 12:36:19 -0700

    big overhaul using zipper for lines/prefixes

--- src/wevre/pptree/main.clj
+++ src/wevre/pptree/main.clj
@@ -3,7 +3,8 @@
             [clojure.java.io :as io]
             [wevre.pptree.cli :as cli]
             [wevre.natural-compare :refer [natural-compare]]
-            [clojure.walk :as w])
+            [clojure.walk :as w]
+            [clojure.zip :as z])
   (:gen-class))
 
 (def ^:dynamic *sep* "/")
@@ -12,120 +13,186 @@
 (def ^:dynamic *dirs-last* false)
 (def ^:dynamic *lex-sort* false)
 
-(defn split-last 
+(defn split-last
   "Split path at final *sep*."
   [s]
   (if-let [i (str/last-index-of s *sep*)]
     (map str/join (split-at (inc i) s))
     (list "" s)))
 
+(defprotocol Prefixed
+  "Items (paths or trees) that contain removable prefix."
+  (deprefix [val pfx] "Remove prefix")
+  (root [val] "Path at root of item")
+  (children [val])
+  (as-tree [val] "Wrap item into seq if necessary")
+  (as-child [val] "Unwrap 1-element seqs")
+  (dir? [val] "True if item is a directory")
+  (split [val] "Split path if it is not a directory")
+  (sort-childs [val f] "Sort children with f"))
+
+(extend-protocol Prefixed
+  java.lang.String
+   (deprefix [val pfx] (str/replace-first val pfx ""))
+   (root [val] val)
+   (children [val] ())
+   (as-tree [val] (list val))
+   (as-child [val] val)
+   (dir? [val] (str/ends-with? val *sep*))
+   (split [val] 
+     (let [[head tail] (split-last val)] 
+       (if (and (seq head) (seq tail)) (list head tail) val)))
+   (sort-childs [val f] val)
+  
+  clojure.lang.Seqable
+   (deprefix [val pfx] (cons (str/replace-first (first val) pfx "") (rest val)))
+   (root [val] (first val))
+   (children [val] (rest val))
+   (as-tree [val] val)
+   (as-child [val] (if (= 1 (count val)) (first val) val))
+   (dir? [val] (or (< 0 (count val)) (dir? (first val))))
+   (split [val] val)
+   (sort-childs [val f] (if (< 1 (count val)) (cons (first val) (f (rest val))) val))
+  
+  nil
+   (deprefix [val pfx] nil)
+   (root [val] nil)
+   (children [val] nil)
+   (as-tree [val] nil)
+   (as-child [val] nil)
+   (dir? [val] nil)
+   (split [val] nil)
+   (sort-childs [val f] nil))
+
 (defn common-prefix
   "Return common prefix ending with *sep*."
   [a b]
-  (->> (map vector a b)
+  (->> (map vector (root a) (root b))
        (take-while (partial apply =))
        (map first)
        (apply str)
        split-last
-       first))
+       root))
 
-(defprotocol Prefixed
-  "Items (paths or trees) that contain a removable prefix."
-  (deprefix [val pfx]))
-
-(extend-protocol Prefixed
-  java.lang.String
-  (deprefix [val pfx] (str/replace-first val pfx ""))
-  clojure.lang.PersistentVector
-  (deprefix [val pfx] (update val 0 #(str/replace-first % pfx ""))))
+(defn re-root
+  "Root tree and path under new, common-prefix parent."
+  [tree path pfx]
+  (cons pfx (->> (list tree path)
+                 (map #(deprefix % pfx))
+                 (map as-child)
+                 (sort-by root))))
 
 (defn add-path
-  "Recursively add str `path` to vec `tree` and return new tree vector.
-   Paths must be added in sorted order."
-  [[par & _ :as tree] path]
-  (let [pfx (common-prefix par path), de-path (deprefix path pfx)]
-    (cond
-      ; Initialize empty tree.
-      (empty? tree) [path]
-      ; Place par and path as siblings under new parent.
-      (pos? (compare par pfx)) [pfx (deprefix tree pfx) [de-path]]
-      ; Ignore duplicate directories.
-      (= path pfx) tree
-      ; Add path as last child.
-      (or (= 1 (count tree)) 
-          (empty? (common-prefix (first (peek tree)) de-path)))
-      (conj tree [de-path])
-      ; Recursively add path to last child.
-      :else
-      (conj (pop tree) (add-path (peek tree) de-path)))))
+  "Return tree with path added."
+  [tree path]
+  (letfn [(insert-down
+           [[par & chs :as tree] path]
+           (let [pfx (common-prefix par path)]
+             #_(prn ["down" par chs tree path pfx])
+             (cond
+               (empty? tree) (as-tree path)
+               (pos? (compare par pfx)) (re-root tree path pfx)
+               (= path pfx) tree
+               :else (cons par (insert-across chs (deprefix path pfx))))))
+          (insert-across
+           [[ch & chs :as childs] path]
+           (let [pfx (common-prefix ch path)]
+             #_(prn ["across" ch chs tree path pfx])
+             (cond
+               (nil? ch) (list path)
+               (seq pfx) (cons (insert-down (as-tree ch) path) chs)
+               (neg? (compare (root path) (root ch))) (cons path childs)
+               (= (root path) (root ch)) childs
+               :else (cons ch (insert-across chs path)))))]
+    (insert-down tree path)))
+
+(comment
+  (reduce add-path () ["/hello/alpha" "/charlie"])
+  (add-path ["/h/w"] "/h/w/d")
+  (add-path ["/h/"] "/h/a")
+  (add-path '("/l/" ("h/a/" "c")) "/l/h/b")
+  (reduce add-path () ["/l/" "/l/h/a" "/l/h/b"])
+  (add-path '("/h/" "a" ("b/" "c")) "/h/b/d")
+  (add-path '("/h/c/") "/h/c/")
+  (add-path '("/h/" ("a/b")) "/h/a/c")
+  ;
+  )
 
 (def spa "    ")
 (def bra "│   ")
 (def tee "├── ")
 (def lst "└── ")
 
-(defn dir? [[par & chs]]
-  (or (seq chs) (and par (str/ends-with? par *sep*))))
-
 (defn sort-chs
-  "Sorts children using each one's first entry, optionally (1) ignoring  
-   case and (2) placing dirs first or last, if requested."
+  "Sorts children by root, optionally (1) ignoring case, (2) placing dirs 
+   first or last, and (3) sorting lexically (as opposed to naturally)."
   [chs]
-  (let [filter-ci (fn [ch] (if *case-sens* ch (update ch 0 str/lower-case)))
-        dir-sorter
-        (fn [dpre fpre]
-          (fn [ch] (update ch 0 #(str (if (dir? ch) dpre fpre) %))))
+  (let [filter-ci (fn [p] (if *case-sens* p (str/lower-case p)))
+        dir-sorter (fn [dpre fpre] (fn [p] (str (if (dir? p) dpre fpre) p)))
         sort-dir (cond
                    *dirs-first* (dir-sorter "0" "1")
                    *dirs-last* (dir-sorter "1" "0")
-                   :else identity)]
-    (sort-by (comp first filter-ci sort-dir) 
-             (if *lex-sort* compare natural-compare) 
-             chs)))
+                   :else identity)
+        key-fn (comp filter-ci sort-dir root)
+        comp (if *lex-sort* compare natural-compare)]
+    (sort-by key-fn comp chs)))
 
-(defn split-lone-ch
-  "Splits root-only tree if not a directory."
-  [[par & chs :as tree]]
-  (let [[head tail] (split-last par)]
-    (if (and (empty? chs) (seq head) (seq tail)) [head [tail]] tree)))
-
-(defn unwind-prefix [[s p & d]]
-  (str (str/join (map #(if % spa bra) (reverse d))) 
-       (when-not (nil? p) (if p lst tee)) 
-       s))
-
-(defn get-prefixes
-  "Transforms nested vector tree into list of paths+prefixes flags.
-   Prefixes are in reverse order (built up with `conj`) and have to be reversed
-   when unwound."
-  [stack [par & chs]]
-  (when par
-    (let [last? (fn [i] (= i (dec (count chs))))]
-      (cons (conj stack par)
-            (mapcat (fn [c i] (get-prefixes (conj stack (last? i)) c)) 
-                    (sort-chs chs)
-                    (range))))))
-
-(defn lines<-tree
-  [tree]
-  (->> tree
-       (w/postwalk #(if (vector? %) (split-lone-ch %) %))
-       (get-prefixes ()) 
-       (map unwind-prefix)))
+(defn prefixes<-lasts [lasts]
+  (map-indexed (fn [i l] (if (zero? i) (if l lst tee) (if l spa bra))) lasts))
 
 (comment
-  (let [input ["/h/l/a" "/h/l/b" "/h/l/c" "/h/w/p" "/h/w/q" "/h/w/q/r" "/h/w/q/s" "/h/w/y"]]
-    (->> input sort (reduce add-path []) lines<-tree))
-  (let [input ["/h/a" "/h/a/b" "/h/a/b/c"]]
-    (->> input sort (reduce add-path []) lines<-tree))
-  
-  (let [input ["/hello/alpha/charlie/file2.txt"
-                 "/hello/alpha/charlie/file10.txt"
-                 "/hello/alpha/Epsilon"
-                 "/hello/alpha/delta"
-                 "/hello/alpha/bravo"
-                 "/hello/alpha/charlie/file1.txt"]]
-    (->> input sort (reduce add-path []) lines<-tree)))
+  (let [input ["/h/a" "/h/a/b" "/h/a/b/c" "/h/d"]
+        tree (->> input (reduce add-path []) (w/postwalk split) (w/postwalk #(sort-childs % sort-chs)))
+        zipper (z/seq-zip tree)
+        zseq (take-while (complement z/end?) (iterate z/next zipper))
+        up-count (fn [loc] (count (take-while (complement nil?) (drop 1 (iterate z/up loc)))))
+        root? (fn [loc] (nil? (z/left loc)))
+        last? (fn [loc] (nil? (->> loc z/right)))
+        up-lasts (fn [loc] (map last? (take-while (complement nil?) (iterate z/up loc))))
+        lasts (fn [loc]
+                (let [depth (up-count loc)]
+                  (cond
+                    (not (dir? (z/node loc))) (take depth (up-lasts loc))
+                    (< 1 depth) (take (dec depth) (drop 1 (up-lasts loc)))
+                    :else ())))
+        prefixes (fn [loc] (reverse (prefixes<-lasts (lasts loc))))]
+    (prn tree)
+    (map (juxt z/node prefixes) (filter (complement z/branch?) zseq)))
+  ;
+  )
+
+(defn lines<-tree
+  "Return seq of printable lines from given tree. Sort or split Tree should be sorted and any
+   lone directories split."
+  [tree]
+  (let [up-count (fn [loc] (count (take-while (complement nil?) (drop 1 (iterate z/up loc)))))
+        last? (fn [loc] (nil? (z/right loc)))
+        up-lasts (fn [loc] (map last? (take-while (complement nil?) (iterate z/up loc))))
+        get-prefixes (fn [loc] (let [depth (up-count loc)]
+                                 (cond
+                                   (not (dir? (z/node loc))) (take depth (up-lasts loc))
+                                   (< 1 depth) (take (dec depth) (drop 1 (up-lasts loc)))
+                                   :else ())))
+        get-line (fn [loc] (str (str/join (reverse (prefixes<-lasts (get-prefixes loc)))) (z/node loc)))]
+    (->> (z/seq-zip tree)
+         (iterate z/next)
+         (take-while (complement z/end?))
+         (filter (complement z/branch?))
+         (map get-line))))
+
+(comment
+  (let [input ["/h/a" "/h/a/b" "/h/a/b/c" "/h/d"]
+        tree (->> input (reduce add-path []) (w/postwalk split) (w/postwalk #(sort-childs % sort-chs)))]
+    (lines<-tree tree))
+  (lines<-tree nil)
+  ;
+  )
+
+(defn tree<-input [inp]
+  (->> (reduce add-path () inp) 
+       (w/postwalk split) 
+       (w/postwalk #(sort-childs % sort-chs))
+       seq))
 
 (defn unwrap-quotes [s]
   (if (and (str/starts-with? s "\"") (str/ends-with? s "\""))
@@ -133,7 +200,7 @@
     s))
 
 (defn -main [& args]
-  (let [{:keys [::cli/exit-message ::cli/ok? ::cli/options ::cli/separator]} (cli/validate-args args)]
+  (let [{:keys [:exit-message :ok? :options :separator]} (cli/validate-args args)]
     (if exit-message
       (cli/exit (if ok? 0 1) exit-message)
       (binding [*sep* (or separator *sep*)
@@ -146,7 +213,14 @@
                          (map str/trim)
                          (map unwrap-quotes)
                          (filter seq)
-                         sort
-                         (reduce add-path [])
+                         tree<-input
                          lines<-tree)]
-            (println (str/join x))))))))
+            (println x)))))))
+
+(comment
+  ;; Not sure why, but without seq we get "()"
+  (let [input ""] (doseq [x (->> input tree<-input     lines<-tree)] (println x)))
+  (let [input ""] (doseq [x (->> input tree<-input seq lines<-tree)] (println x)))
+  ;; Note: moved seq to end of tree<-input
+  ;
+  )
--- test/wevre/pptree/main_test.clj
+++ test/wevre/pptree/main_test.clj
@@ -1,7 +1,6 @@
 (ns wevre.pptree.main-test
   (:require [wevre.pptree.main :as sut]
-            [clojure.test :refer [deftest are is testing]]
-            [clojure.string :as str]))
+            [clojure.test :refer [deftest are is testing]]))
 
 (deftest pptree
   (testing "split last"
@@ -28,37 +27,47 @@
       "/hello/world/charlie" "/hello/world/chicago" "/hello/world/"
       "/hello/world/delta"   "/hello/test/echo"     "/hello/"
       "hello/world/foxtrot"  "frank/george/henry"   ""))
-
-  (testing "add-path"
-    (is (= ["/hello"]
-           (sut/add-path [] "/hello"))
+  
+  (testing "add path"
+    (is (= ["/hello/alpha"]
+           (sut/add-path [] "/hello/alpha"))
         "-- empty tree")
-    (is (= ["/hello/" ["alfa"] ["bravo"]]
+    (is (= ["/hello/" "alfa" "bravo"]
            (sut/add-path ["/hello/alfa"] "/hello/bravo"))
-        "-- prefix shorter than parent")
-    (is (= ["" ["hello"] ["world"]]
+        "-- re-root under shared parent")
+    (is (= ["/hello/" "alfa" ["bravo/" "charlie"]]
+           (sut/add-path ["/hello/bravo/" "charlie"] "/hello/alfa"))
+        "-- re-root before")
+    (is (= ["/hello/" ["alfa/" "charlie"] "delta"]
+           (sut/add-path ["/hello/alfa/" "charlie"] "/hello/delta"))
+        "-- re-root after")
+    (is (= ["" "hello" "world"]
            (sut/add-path ["hello"] "world"))
-        "-- no common prefix")
+        "-- re-root with empty prefix")
     (is (= ["/hello/charlie/"]
            (sut/add-path ["/hello/charlie/"] "/hello/charlie/"))
         "-- path == prefix (duplicate)")
-    (is (= ["/hello/world/" ["delta"]]
-           (sut/add-path ["/hello/world/"] "/hello/world/delta"))
-        "-- childless tree")
-    (is (= ["/hello/" ["alfa"] ["bravo"] ["charlie"]]
-           (sut/add-path ["/hello/" ["alfa"] ["bravo"]] "/hello/charlie"))
-        "-- no common prefix with last child, add as next child")
-    (is (= ["/hello/" ["alfa"] ["bravo/" ["charlie"] ["delta"]]]
-           (sut/add-path ["/hello/" ["alfa"] ["bravo/charlie"]]
-                         "/hello/bravo/delta"))
-        "-- add to and replace last child"))
+    (is (= ["/hello/" "alpha"]
+           (sut/add-path ["/hello/"] "/hello/alpha"))
+        "-- first-ever child")
+    (is (= ["/hello/" "alpha" ["bravo/" "charlie" "delta"]]
+            (sut/add-path ["/hello/" "alpha" ["bravo/" "charlie"]]
+                           "/hello/bravo/delta"))
+        "-- insert into existing child")
+    (is (= ["/hello/" "alfa" "bravo" "charlie"]
+           (sut/add-path ["/hello/" "alfa" "bravo" "charlie"]
+                          "/hello/bravo"))
+        "-- don't add duplicate child")
+    (is (= ["/hello/" "alfa" "bravo" "charlie"]
+           (sut/add-path ["/hello/" "bravo" "charlie"] "/hello/alfa"))
+        "-- add as first child")
+    (is (= ["/hello/" "alfa" "bravo" "charlie"]
+           (sut/add-path ["/hello/" "alfa" "charlie"] "/hello/bravo"))
+        "-- add as middle child")
+    (is (= ["/hello/" "alfa" "bravo" "charlie"]
+           (sut/add-path ["/hello/" "alfa" "bravo"] "/hello/charlie"))
+        "-- add as last child"))
   
-  (testing "split lone child"
-    (is (= ["/h/"] (sut/split-lone-ch ["/h/"])))
-    (is (= ["/h/q/" ["r"]] (sut/split-lone-ch ["/h/q/r"])))
-    (is (= ["/h/" ["q"]] (sut/split-lone-ch ["/h/" ["q"]])))
-    (is (= ["/h/" ["q"]] (sut/split-lone-ch ["/h/" ["q"]]))))
-
   (testing "lines from tree"
     (is
      (= ["/h/"
@@ -72,16 +81,16 @@
          "    └── q/"
          "        └── r"]
         (sut/lines<-tree
-         ["/h/" ["l/" ["a"] ["b"] ["c"]] ["w/" ["p"] ["q"] ["q/r"]]]))))
+         '("/h/" ("l/" "a" "b" "c") ("w/" "p" "q" ("q/" "r")))))))
   
-  (testing "sort DIR first"
+  (testing "test options"
     (let [input ["/hello/alpha/charlie/file2.txt"
                  "/hello/alpha/charlie/file10.txt"
                  "/hello/alpha/Epsilon"
                  "/hello/alpha/delta"
                  "/hello/alpha/bravo"
                  "/hello/alpha/charlie/file1.txt"]
-          get-lines (fn [inp] (sut/lines<-tree (sut/tree<-paths inp)))]
+          get-lines (fn [inp] (sut/lines<-tree (sut/tree<-input inp)))]
       (is (= (get-lines input)
              ["/hello/alpha/"
               "├── bravo"
